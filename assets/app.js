47a9a444435f61c2957bacef136f9a935a6e04e4
mtblueocean <mtblueocean@gmail.com>
2019-01-31 04:36:36 -0600

    update sort collection function

--- assets/app.js
+++ assets/app.js
@@ -220,17 +220,17 @@
 		    },
 		    mouseleave: function(){
 			    var me = $(this);
-			    me.find('.img2').hide().prev('.img1').show();
+			    me.find('.img2').hide().prev('.img1').show();		    
 		    }
 		  });	
 	  }
 	  	
 	}	
 	
-	function slickInit() {
+	function slickInit(){
 		var $slick = $('.slick');
-
-		if ($slick.length > 0) {
+		
+		if($slick.length > 0){
 			$('.slick').slick();
 		}
 	};
@@ -515,30 +515,30 @@
       open_mailing_list_popup = true;
     }
     
-    /*$('.mail-trigger a').click(function(e){
-        $('body').removeClass('sizeguide-open');
-      	e.preventDefault();
+  //   $('.mail-trigger a').click(function(e){
+  //       $('body').removeClass('sizeguide-open');
+  //     	e.preventDefault();
 
-	    $.fancybox.open([{
-  			width: 700,
-  			height: 700,
-  			href: '#newsBox3'
-  		}], {
-  			padding: 50
-  		});
-    });
+	 //    $.fancybox.open([{
+		// 	width: 700,
+		// 	height: 700,
+		// 	href: '#newsBox3'
+		// }], {
+		// 	padding: 50
+		// });
+  //   });
     
-    if(open_mailing_list_popup){
-	    setTimeout(function(){		
-		    $.fancybox.open([{
-			    width: 700,
-			    height: 700,
-			    href: '#newsBox3'
-		    }], {
-			    padding: 50
-		    });
-		  }, 500);
-	  }*/
+   //  if(open_mailing_list_popup){
+	  //   setTimeout(function(){		
+		 //    $.fancybox.open([{
+			//     width: 700,
+			//     height: 700,
+			//     href: '#newsBox3'
+		 //    }], {
+			//     padding: 50
+		 //    });
+		 //  }, 500);
+	  // }
     
     // init functions
     showPage();
@@ -570,20 +570,20 @@
   });
   
   $(document).ready(function(){
-    $('form#create_customer').submit(function() {
-		  fbq('track', 'CompleteRegistration');
+    $('form#create_customer').submit(function(){
+		fbq('track', 'CompleteRegistration');
     });
     
-    $('#footer-newsletter-2 form').submit(function() {
-		  fbq('track', 'Lead', {
-      	source: 'footer'
-      });
+    $('#footer-newsletter-2 form').submit(function(){
+		fbq('track', 'Lead', {
+        	source: 'footer'
+        });
     });
     
-    $('#popup-newsletter-2 form').submit(function() {
-		  fbq('track', 'Lead', {
-      	source: 'popup'
-      });
+    $('#popup-newsletter-2 form').submit(function(){
+		fbq('track', 'Lead', {
+        	source: 'popup'
+        });
     });
     
     var imageHeight = $('#home-featured #featured-collection-1 img').height();
@@ -611,11 +611,17 @@
     $('form.search').find('input[type=text]').focus(function() { 
     	$(this).parent('form').addClass('expanded');
     });
+
     $('form.search').find('input[type=text]').blur(function() { 
     	$(this).parent('form').removeClass('expanded');
     });
   
-    
+    // Filter/Sort Menu Items Effect
+    $('.sort-controls .sort-item').on('click', function() {
+      $(this).parents('.sort-controls').find('.sort-item').removeClass('active');
+      $(this).toggleClass('active');
+    });
+
     $('#footer-newsletter-2asdf form, #popup-newsletter-2asdf form').submit(function(e) {
         e.preventDefault();
         var email = $(this).find('input[type="email"]').val();
